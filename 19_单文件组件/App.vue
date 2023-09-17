ab42a6123450108231d8f9c21cea4267bff8c4cb
CuanXxx <nc12300.svip@qq.com>
2023-09-17 22:46:17 +0800

    提交

---
+++ 19_单文件组件/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <School></School>
+    <Student></Student>
+  </div>
+</template>
+
+<script>
+  import School from './School'
+  import Student from './Student'
+
+  export default {
+    name: 'App',
+    data() {
+      return {}
+    },
+    components: {
+      School,
+      Student,
+    },
+  }
+</script>
+<style></style>
---
+++ 19_单文件组件/School.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <h2 class="demo">学校名称：{{ name }}</h2>
+    <h2>学校地址：{{ address }}</h2>
+    <button @click="showName">弹窗</button>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'School',
+    data() {
+      return {
+        name: '江西科技学院',
+        address: '江西',
+      }
+    },
+    methods: {
+      showName() {
+        alert(this.name)
+      },
+    },
+  }
+</script>
+
+<style>
+  .demo {
+    background-color: pink;
+  }
+</style>
